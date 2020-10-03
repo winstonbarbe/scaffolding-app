9de9768f75c60e140b09f9d41e9c4134c3aba0f4
winstonbarbe <winston.barbe@gmail.com>
2020-10-03 15:54:18 -0400

    Adds scaffolding for Posts and a home action

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  resources :posts
+  # EXAMPLE HTML ROUTE
+  # get "/photos" => "photos#index"
+
+  # EXAMPLE JSON ROUTE WITH API NAMESPACE
+  # namespace :api do
+  #   get "/photos" => "photos#index"
+  # end
+end
